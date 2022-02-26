f1b21d9ae24aafc5f5d4c3fdde660602180a15b1
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2022-02-26 11:31:53 +0700

    feat: first init

---
+++ src/com/ww/BrowserApp.java
@@ -0,0 +1,13 @@
+package com.ww;
+
+public class BrowserApp {
+    public static void main(String[] args) {
+        System.out.println("Hello progjar");
+        String link;
+        String[] links; // = class.static_method();
+        String[] anchor_links;
+        String[] downloadable_links;
+        // class.static_method(downloadable_links);
+
+    }
+}
