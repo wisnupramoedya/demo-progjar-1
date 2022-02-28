587155f8f4de35d0f4494879d60b97826c9d27bd
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2022-02-28 15:22:23 +0700

    feat: add get basic method

--- src/com/ww/BrowserApp.java
+++ src/com/ww/BrowserApp.java
@@ -1,10 +1,18 @@
 package com.ww;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class BrowserApp {
     public static void main(String[] args) {
         System.out.println("Hello progjar");
-        String link;
-        String[] links; // = class.static_method();
+        try {
+            String html = BrowserTextGenerator.openWeb();
+            List<String> links = BrowserTextGenerator.getAllLinks(html);
+        } catch (Exception e) {
+            Logger.getLogger(BrowserApp.class.getName()).log(Level.SEVERE, null, e);
+        }
         String[] anchor_links;
         String[] downloadable_links;
         // class.static_method(downloadable_links);
---
+++ src/com/ww/BrowserTextGenerator.java
@@ -0,0 +1,23 @@
+package com.ww;
+
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BrowserTextGenerator {
+    public static String openWeb() throws IOException, URISyntaxException {
+        return null;
+    }
+
+    public static List<String> getAllLinks(String html) {
+        return null;
+    }
+}
